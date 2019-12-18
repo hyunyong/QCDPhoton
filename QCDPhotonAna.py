97baede704daa94be45c045603908f4f5554ce9b
hyunyong <antilogy33@gmail.com>
2019-12-19 07:15:46 +0900

    first commit

---
+++ QCDPhotonAna.py
@@ -0,0 +1,14 @@
+from ROOT import *
+gROOT.SetBatch(1)
+gStyle.SetOptStat(0)
+
+tf = TFile("QCDPhoton.root")
+tr = tf.Get("QCDPhoton")
+
+for e in tr:
+  print "Event number: ", e.evn
+  print "  Number of jet: ", e.nJet
+  for i, pJet in enumerate(e.photon):
+     print "    Number of photon at jet %d: "%i, len(pJet)
+     for j, p in enumerate(pJet):
+       print "      %d photon 4 vec.: "%j, p.Px(), p.Py(), p.Pz(), p.Pt()
---
+++ README.md
@@ -0,0 +1,12 @@
+# QCD photon analyzer
+
+```
+git clone git@github.com:hyunyong/QCDPhoton.git
+python QCDPhotonAna.py
+```
+or 
+```
+wget https://raw.githubusercontent.com/hyunyong/QCDPhoton/master/QCDPhotonAna.py
+wget https://raw.githubusercontent.com/hyunyong/QCDPhoton/master/QCDPhoton.root
+python QCDPhotonAna.py
+```
