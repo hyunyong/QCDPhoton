35f0507d249ee1547424b6ec72e86a546111f088
hyunyong <antilogy33@gmail.com>
2019-12-19 07:35:55 +0900

    2nd commit

--- QCDPhotonAna.py
+++ QCDPhotonAna.py
@@ -5,6 +5,7 @@ gStyle.SetOptStat(0)
 tf = TFile("QCDPhoton.root")
 tr = tf.Get("QCDPhoton")
 
+outFile = open("photonM.txt", "w")
 for e in tr:
   print "Event number: ", e.evn
   print "  Number of jet: ", e.nJet
@@ -12,3 +13,4 @@ for e in tr:
      print "    Number of photon at jet %d: "%i, len(pJet)
      for j, p in enumerate(pJet):
        print "      %d photon 4 vec.: "%j, p.Px(), p.Py(), p.Pz(), p.Pt()
+       outFile.write("%f %f %f %f\n"%(p.Px(), p.Py(), p.Pz(), p.E()))
